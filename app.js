9b6cb0474a47cc3b190fb7fbc8d50bc4c6dfa35c
pirmansh <fsyah1362@gmail.com>
2020-03-29 00:36:55 +0700

    edit UI

--- app.js
+++ app.js
@@ -22,15 +22,13 @@ function getApi(){
             let deaths= '';
             let activeCare= '';
             let lastUpdatedAt= '';
-
-                   
+               
                     confirmed += `<h3>${response.confirmed.value}</h3>`
                     recovered += `<h3>${response.recovered.value}</h3>`
                     deaths += `<h3>${response.deaths.value}</h3>`
                     activeCare += `<h3>${response.activeCare.value}</h3>`
-                    lastUpdatedAt += `<small class="color">${response.metadata.lastUpdatedAt}</small>`
-       
-
+                    lastUpdatedAt += `<small class="lastupdate">${response.metadata.lastUpdatedAt}</small>`
+    
             document.getElementById('output').innerHTML = confirmed;
             document.getElementById('activeCare').innerHTML = activeCare;
             document.getElementById('recovered').innerHTML = recovered;
