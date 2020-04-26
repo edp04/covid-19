beb19c500f68355e40f2c9bcc46c8027411df711
pirmansh <fsyah1362@gmail.com>
2020-04-27 00:04:12 +0700

    editing data

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ function getApi(){
 
             const response = JSON.parse(this.responseText);
             
-            
             let confirmed = '';
             let recovered= '';
             let deaths= '';
@@ -39,3 +38,47 @@ function getApi(){
     xhr.send();
 }
  const createClock = setInterval(getApi, 1000);
+
+
+ function getDataProvinsiItem(){
+
+    const baseUrl = 'https://api.kawalcorona.com/indonesia/provinsi/';
+
+
+     const xhr = new XMLHttpRequest;
+
+     xhr.open('GET', `${baseUrl}`, true);
+
+     xhr.onload = function(){
+         if(this.status === 200){
+            let option = document.getElementById('provinsi-item');
+            const response = JSON.parse(this.responseText);
+            const td = document.getElementById('hasil');
+            let counter = 1;
+            response.forEach(result => {
+                option.innerHTML += `<option value="${result.attributes.Provinsi}">${result.attributes.Provinsi}</option>`;
+
+                td.innerHTML += `
+                    <tr>
+                    <th id="urutan" scope="row">${counter++}</th>
+                    <td>${result.attributes.Provinsi}</td>
+                    <td>${result.attributes.Kasus_Posi}</td>
+                    <td>${result.attributes.Kasus_Semb}</td>
+                    <td>${result.attributes.Kasus_Meni}</td>
+                    </tr>
+                `;
+                // const prov = document.getElementById('provinsi-item');
+                // prov.addEventListener('keyup', function(){
+                //     const provinsi = document.getElementById('provinsi');
+
+                //     provinsi.innerText = result.attributes.Provinsi;
+                // });
+            })
+         }
+     }
+
+     xhr.send();
+ }
+
+getDataProvinsiItem();
+
